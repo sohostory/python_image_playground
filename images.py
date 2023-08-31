7963e1e3385f12d723a0bb438879e51ff91014d3
Hoyoung Lee <sohostory@gmail.com>
2023-08-31 08:30:49 -0500

    test 3

--- images.py
+++ images.py
@@ -1,16 +1,5 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
-# filtered_img = img.filter(ImageFilter.SMOOTH)
-converted_img = img.convert('L')
-
-# filtered_img.save("blur.png", 'png')
-
-box = (100, 100, 400, 400)
-region = converted_img.crop(box)
-rotated_img = region.rotate(90)
-
-resized_img = rotated_img.resize((300, 300))
-
-resized_img.save('grey.png', 'png')
-resized_img.show()
+img = Image.open('./astro.jpg')
+img.thumbnail((400,400))
+img.save('thumnail.jpg')
