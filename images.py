f30ffe6a23ebd717162e29a5487fc1e13e030437
Hoyoung Lee <sohostory@gmail.com>
2023-08-31 08:25:57 -0500

    test 2

--- images.py
+++ images.py
@@ -5,4 +5,12 @@ img = Image.open('./Pokedex/pikachu.jpg')
 converted_img = img.convert('L')
 
 # filtered_img.save("blur.png", 'png')
-converted_img.save('grey.png', 'png')
+
+box = (100, 100, 400, 400)
+region = converted_img.crop(box)
+rotated_img = region.rotate(90)
+
+resized_img = rotated_img.resize((300, 300))
+
+resized_img.save('grey.png', 'png')
+resized_img.show()
